56067e2ca4222afab0aae6e749d0fecb746d9239
Chris Eason <netchris@gmail.com>
2018-10-20 16:37:25 -0400

    Format file with spaces

--- examples/src/server.py
+++ examples/src/server.py
@@ -7,20 +7,20 @@ SERVER_MESSAGE = os.environ.get("CUSTOM_SERVER_MESSAGE", "<no CUSTOM_SERVER_MESS
 
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-	def do_GET(s):
-		print("Received HTTP request")
-		s.send_response(200)
-		s.send_header("Content-type", "text/text")
-		s.end_headers()
-		s.wfile.write(SERVER_MESSAGE)
+    def do_GET(s):
+        print("Received HTTP request")
+        s.send_response(200)
+        s.send_header("Content-type", "text/text")
+        s.end_headers()
+        s.wfile.write(SERVER_MESSAGE)
 
 
 if __name__ == '__main__':
-	server_class = BaseHTTPServer.HTTPServer
-	print("Starting HTTP server on port %d" % PORT_NUMBER)
-	httpd = server_class(("0.0.0.0", PORT_NUMBER), MyHandler)
-	try:
-		httpd.serve_forever()
-	except KeyboardInterrupt:
-		pass
-	httpd.server_close()
+    server_class = BaseHTTPServer.HTTPServer
+    print("Starting HTTP server on port %d" % PORT_NUMBER)
+    httpd = server_class(("0.0.0.0", PORT_NUMBER), MyHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
