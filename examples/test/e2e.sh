840e35e04c2c5be4f733b9aec518181fd8bd8374
Chris Eason <netchris@gmail.com>
2018-11-06 12:10:44 -0500

    Make test less flaky

--- examples/test/e2e.sh
+++ examples/test/e2e.sh
@@ -6,6 +6,18 @@ trap err_report ERR
 
 source ${BUILD_WORKING_DIRECTORY:="."}/.rules_terraform/test_vars.sh
 
+while true; do
+	if http_code=$(curl -s -o /dev/null -L -w "%{http_code}" "$SERVICE_URL"); then
+		:
+	fi
+	if [ "$http_code" == "200" ]; then
+		break
+	else
+		>&2 echo "Could not connect to server, waiting a bit.."
+		sleep 2
+	fi
+done
+
 until response=$(curl -sSL "$SERVICE_URL"); do
 	>&2 echo "Could not connect to server, waiting a bit.."
 	sleep 2
